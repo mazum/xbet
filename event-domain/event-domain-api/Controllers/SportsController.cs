faca8c7cd39c15cb5e502783aa3e6e811ada707f
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2023-02-08 23:53:37 +1100

    feat:sports controller

---
+++ event-domain/event-domain-api/Controllers/SportsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace event_domain_api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SportsController : ControllerBase
+{
+    private readonly ILogger<SportsController> _logger;
+
+    public SportsController(ILogger<SportsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetSports")]
+    public string Get()
+    {
+        return "Sports Controller";
+    }
+}
